9231782076eea1ff65dfb338293ef53f4f6a726b
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-29 18:56:01 +0530

    BUILD 010B STEP08 FILE02 - Historical Integration

---
+++ lib/tokenResolver/historicalResolver.ts
@@ -0,0 +1,16 @@
+﻿import { ensureTokenResolver } from "./bootstrap";
+import { resolveToken } from "./tokenResolver";
+import { InstrumentInfo } from "./types";
+
+export function getHistoricalInstrument(symbol:string):InstrumentInfo{
+    ensureTokenResolver();
+    const info = resolveToken(symbol);
+    if(!info){
+        throw new Error(`Instrument not found: ${symbol}`);
+    }
+    return info;
+}
+
+export function getHistoricalToken(symbol:string):number{
+    return getHistoricalInstrument(symbol).token;
+}
