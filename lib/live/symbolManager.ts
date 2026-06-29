5f29ae0ccf7eb0d49a267ca95d727a976ca91f10
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-29 19:36:44 +0530

    LIVE 002 - Symbol Manager

---
+++ lib/live/symbolManager.ts
@@ -0,0 +1,31 @@
+﻿import { publishTick, getCurrentTick } from "./liveEngine";
+import { resolveToken } from "../tokenResolver";
+
+let currentSymbol = "SBIN";
+
+export function setCurrentSymbol(symbol:string){
+  currentSymbol = symbol.toUpperCase();
+}
+
+export function getCurrentSymbol(){
+  return currentSymbol;
+}
+
+export function getCurrentInstrument(){
+  return resolveToken(currentSymbol);
+}
+
+export function publishCurrentTick(lastPrice:number, volume:number=0){
+  const info = resolveToken(currentSymbol);
+  if(!info) return;
+
+  publishTick({
+    symbol: info.symbol,
+    token: info.token,
+    lastPrice,
+    volume,
+    time: Date.now()
+  });
+}
+
+export { getCurrentTick };
