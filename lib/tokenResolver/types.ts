64ef0f06a85c475f978a3f42659f07f22484da0e
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-29 17:40:33 +0530

    BUILD 010B STEP07 FILE01 V2

---
+++ lib/tokenResolver/types.ts
@@ -0,0 +1,18 @@
+﻿export interface InstrumentInfo {
+  token:number;
+  symbol:string;
+  exchange:string;
+  tradingsymbol:string;
+  tickSize:number;
+  lotSize:number;
+}
+
+export type SymbolMap = Map<string, InstrumentInfo>;
+export type TokenMap = Map<number, InstrumentInfo>;
+
+export interface ResolverState {
+  symbolMap: SymbolMap;
+  tokenMap: TokenMap;
+  loaded: boolean;
+  lastLoaded?: Date;
+}
