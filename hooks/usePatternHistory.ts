d82def4b77da6eba19d190d23002582f26c56cbf
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-30 08:59:17 +0530

    BUILD 003A - Pattern Engine Loader

---
+++ hooks/usePatternHistory.ts
@@ -0,0 +1,8 @@
+﻿"use client";
+
+import { useMemo } from "react";
+import { detectPatterns } from "@/lib/patternEngine/patternLoader";
+
+export function usePatternHistory(candles:any[]) {
+    return useMemo(() => detectPatterns(candles), [candles]);
+}
---
+++ lib/patternEngine/patternLoader.ts
@@ -0,0 +1,6 @@
+﻿import { analyzePattern } from "@/lib/pattern/patternEngine";
+
+export function detectPatterns(candles:any[]) {
+    if (!candles || candles.length === 0) return null;
+    return analyzePattern(candles);
+}
