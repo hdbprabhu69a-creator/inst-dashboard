53cdc2d66ff54f22a66336e702c6b2be118f43ae
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-30 09:14:47 +0530

    BUILD 005B - Realtime Volume Engine

---
+++ lib/liveChart/realtimeVolume.ts
@@ -0,0 +1,11 @@
+﻿import { getLiveCandle, setLiveCandle } from "./liveCandleStore";
+
+export function updateRealtimeVolume(volume:number){
+  const candle=getLiveCandle();
+  if(!candle) return;
+
+  setLiveCandle({
+    ...candle,
+    volume
+  });
+}
