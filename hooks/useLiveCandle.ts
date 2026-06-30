27e97b4cc0e71612c5f4c187dd9835b083283abb
prabhukumar <hdbprabhu69a@gmail.com>
2026-06-30 09:05:10 +0530

    BUILD 004A - Live Candle Foundation

---
+++ hooks/useLiveCandle.ts
@@ -0,0 +1,15 @@
+﻿"use client";
+
+import { useEffect,useState } from "react";
+import { getLiveCandle } from "@/lib/liveChart/liveCandleStore";
+
+export function useLiveCandle(){
+  const [candle,setCandle]=useState(getLiveCandle());
+
+  useEffect(()=>{
+    const id=setInterval(()=>setCandle(getLiveCandle()),500);
+    return ()=>clearInterval(id);
+  },[]);
+
+  return candle;
+}
---
+++ lib/liveChart/liveCandleStore.ts
@@ -0,0 +1,19 @@
+﻿export interface LiveCandle {
+  symbol:string;
+  open:number;
+  high:number;
+  low:number;
+  close:number;
+  volume:number;
+  time:number;
+}
+
+let current:LiveCandle|null=null;
+
+export function setLiveCandle(c:LiveCandle){
+  current=c;
+}
+
+export function getLiveCandle(){
+  return current;
+}
